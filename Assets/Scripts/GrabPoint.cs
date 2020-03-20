52a6a42bd2a03ad306917daf3ea02ab2f8b4c940
Will Setten <willsetten@hotmail.co.uk>
2020-03-20 18:23:22 +0000

    Added Rack and Rail movement

---
+++ Assets/Scripts/GrabPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabPoint : MonoBehaviour
+{
+    public RackMovement rack;
+    public string mode;
+
+    private void OnMouseDown()
+    {
+        rack.moveRack(mode);
+    }
+
+    private void OnMouseUp()
+    {
+        rack.stopRack();
+    }
+}
---
+++ Assets/Scripts/RackMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RackMovement : MonoBehaviour
+{
+    public Transform rail;
+    //True if the rack is being rotated along the rail
+    public bool rotating;
+    //True if the rack is being shifted forward or back
+    public bool shifting;
+    void Start()
+    {
+        rail = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotating)
+        {
+            rail.Rotate(new Vector3(0, Input.GetAxis("Mouse X")*Time.deltaTime*100, 0), Space.Self);
+        }
+        else if (shifting)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x + Input.GetAxis("Mouse Y")* 3 *Time.deltaTime, transform.localPosition.y, transform.localPosition.z);
+        }
+    }
+
+    public void moveRack(string mode)
+    {
+        if (mode == "Rotating")
+        {
+            rotating = true;
+        }
+        else if (mode == "Shifting")
+        {
+            shifting = true;
+        }
+    }
+    public void stopRack()
+    {
+        rotating = false;
+        shifting = false;
+    }
+}
