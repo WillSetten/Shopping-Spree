b05806ad7c2bbbd8f51b356116d1eff6ba3b5d57
NJHewadewa <navin.hewadewa.2017@live.rhul.ac.uk>
2020-03-20 20:24:04 +0000

    clothes display added

---
+++ Assets/Scripts/ClothesDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothesDisplay : MonoBehaviour
+{
+  public GameObject[] clothesArray;
+  public Vector3 start = new Vector3(0, 5, 0), end = new Vector3(2, 5, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clothesArray = Resources.LoadAll<GameObject>("Clothing_Pack/Prefabs");
+
+        int i = 0;
+        foreach(GameObject o in clothesArray) {
+          Debug.Log(o);
+          Instantiate(o, new Vector3( i, 0, 0), Quaternion.identity);
+          i += 3;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
