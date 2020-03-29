09604688b5887dc587b65bd926f6f63f29620247
Will Setten <willsetten@hotmail.co.uk>
2020-03-29 12:53:43 +0100

    Edited clothes spawning positions

--- Assets/Scripts/ClothesDisplay.cs
+++ Assets/Scripts/ClothesDisplay.cs
@@ -6,18 +6,19 @@ public class ClothesDisplay : MonoBehaviour
 {
   public GameObject[] clothesArray;
   public Transform clothesStorage;
+  public int clothesSpacing;
   public Vector3 start = new Vector3(0, 5, 0), end = new Vector3(2, 5, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         clothesArray = Resources.LoadAll<GameObject>("Clothing_Pack/Prefabs");
-
+        clothesSpacing = 2;
         int i = 0;
-        while(i<17){
-          GameObject temp = Instantiate(clothesArray[i], new Vector3( 0, 0, 0), clothesArray[i].transform.rotation);
+        while(i<=9*clothesSpacing){
+          GameObject temp = Instantiate(clothesArray[i], new Vector3( 0, clothesArray[i].transform.position.y, 0), clothesStorage.rotation * clothesArray[i].transform.rotation);
           temp.transform.parent = clothesStorage;
-          temp.transform.localPosition = new Vector3(0,0,(float)i/2);
+          temp.transform.localPosition = new Vector3(0, clothesArray[i].transform.position.y,(float)i/clothesSpacing);
           i += 1;
         }
     }
