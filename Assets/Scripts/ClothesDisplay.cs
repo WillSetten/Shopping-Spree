d2c720e809c9b7bcc7580430c11807ae3e8f91eb
Will Setten <willsetten@hotmail.co.uk>
2020-03-29 16:07:43 +0100

    Added basic clothes scrolling

--- Assets/Scripts/ClothesDisplay.cs
+++ Assets/Scripts/ClothesDisplay.cs
@@ -6,14 +6,14 @@ public class ClothesDisplay : MonoBehaviour
 {
   public GameObject[] clothesArray;
   public Transform clothesStorage;
-  public int clothesSpacing;
+  public float clothesSpacing;
   public Vector3 start = new Vector3(0, 5, 0), end = new Vector3(2, 5, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         clothesArray = Resources.LoadAll<GameObject>("Clothing_Pack/Prefabs");
-        clothesSpacing = 2;
+        clothesSpacing = 1.1f;
         int i = 0;
         while(i<=9*clothesSpacing){
           GameObject temp = Instantiate(clothesArray[i], new Vector3( 0, clothesArray[i].transform.position.y, 0), clothesStorage.rotation * clothesArray[i].transform.rotation);
@@ -26,6 +26,13 @@ public class ClothesDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            clothesStorage.transform.localPosition = new Vector3(clothesStorage.transform.localPosition.x, clothesStorage.transform.localPosition.y, clothesStorage.transform.localPosition.z+ 2*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            clothesStorage.transform.localPosition = new Vector3(clothesStorage.transform.localPosition.x, clothesStorage.transform.localPosition.y, clothesStorage.transform.localPosition.z - 2*Time.deltaTime);
+        }
     }
 }
