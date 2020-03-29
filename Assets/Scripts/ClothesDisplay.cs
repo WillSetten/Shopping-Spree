7267850c100250b1f4eb2e69e76e3a985f9f1f04
NJHewadewa <navin.hewadewa.2017@live.rhul.ac.uk>
2020-03-29 16:51:41 +0100

    scrolling

--- Assets/Scripts/ClothesDisplay.cs
+++ Assets/Scripts/ClothesDisplay.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class ClothesDisplay : MonoBehaviour
 {
-  public GameObject[] clothesArray;
-  public Transform clothesStorage;
-  public float clothesSpacing;
-  public Vector3 start = new Vector3(0, 5, 0), end = new Vector3(2, 5, 0);
+    public GameObject[] clothesArray;
+    public Transform clothesStorage;
+    private MouseLook mouse;
+    private bool scrolling;
+    public float clothesSpacing;
 
     // Start is called before the first frame update
     void Start()
     {
+        mouse = FindObjectOfType<MouseLook>();
+        scrolling = false;
+
         clothesArray = Resources.LoadAll<GameObject>("Clothing_Pack/Prefabs");
         clothesSpacing = 1.1f;
+
         int i = 0;
-        while(i<=9*clothesSpacing){
+        while(i < clothesArray.Length){
           GameObject temp = Instantiate(clothesArray[i], new Vector3( 0, clothesArray[i].transform.position.y, 0), clothesStorage.rotation * clothesArray[i].transform.rotation);
           temp.transform.parent = clothesStorage;
           temp.transform.localPosition = new Vector3(0, clothesArray[i].transform.position.y,(float)i/clothesSpacing);
@@ -23,16 +28,21 @@ public class ClothesDisplay : MonoBehaviour
         }
     }
 
+    private void OnMouseDown() {
+        Debug.Log("MOUSEDOWN");
+        scrolling = true;
+    }
+
+    private void OnMouseUp() {
+        scrolling = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            clothesStorage.transform.localPosition = new Vector3(clothesStorage.transform.localPosition.x, clothesStorage.transform.localPosition.y, clothesStorage.transform.localPosition.z+ 2*Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            clothesStorage.transform.localPosition = new Vector3(clothesStorage.transform.localPosition.x, clothesStorage.transform.localPosition.y, clothesStorage.transform.localPosition.z - 2*Time.deltaTime);
+        if(scrolling) {
+            Debug.Log("MOUSESCROLLING");
+            clothesStorage.localPosition = new Vector3(clothesStorage.localPosition.x, clothesStorage.localPosition.y , clothesStorage.localPosition.z - Input.GetAxis("Mouse X")* mouse.mouseSensitivity/10 *Time.deltaTime);
         }
     }
 }
