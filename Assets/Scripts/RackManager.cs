753c3b87bb385ea2dd60be9c6f37c2ec86d0b1e5
Will Setten <willsetten@hotmail.co.uk>
2020-04-02 15:17:55 +0100

    Implemented Rack addition and deletion
    Bound addRack to the 'O' key for now.

---
+++ Assets/Scripts/RackManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RackManager : MonoBehaviour
+{
+    public GameObject rail;
+    public Transform player;
+    public List<Transform> racks;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(player.position.x, -2, player.position.z);
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            addRack();
+        }
+    }
+
+    //Adds a new rack where the player is facing
+    public void addRack(){
+        Vector3 rotation = player.rotation.eulerAngles;
+        rotation = new Vector3(rotation.x, rotation.y-90, rotation.z);
+
+        racks.Add(Instantiate(rail, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.Euler(rotation)).GetComponent<Transform>());
+    }
+
+    //Deletes a given rack from the space
+    public void deleteRack(Transform rack)
+    {
+        rack.gameObject.SetActive(false);
+        racks.Remove(rack);
+    }
+}
